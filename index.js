03aec6d0834836707995e6bab5b53b31fc93fb10
eric <contreras.eric18@gmail.com>
2021-07-27 16:27:05 -0400

    commit tree 2485f1396b814f72266e3ac0e07c5ebb6426a857

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Add your code here
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+  
+//   let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     }); 
+
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json", 
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(resp => resp.json())
+    .then(resp => {
+        let newElement = document.createElement('e');
+        newElement.innerHTML = resp.id;
+        document.querySelector("body").appendChild(newElement);
+    })
+    .catch(error => {
+        let newElement = document.createElement('e');
+        newElement.innerHTML = error.message;
+        document.querySelector("body").appendChild(newElement);
+    })
+}
